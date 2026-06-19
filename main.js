7ca87012d80db5ebb1572aacf1ec734c6b3fc0e8
anshaer <158002425+anshaer@users.noreply.github.com>
2026-06-19 19:07:13 +0800

    Clean up main.js by removing whitespace and comments
    Removed unnecessary whitespace and comments.

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 const { app, BrowserWindow, globalShortcut, ipcMain } = require('electron');
-
 let mainWindow;
 let isClickThrough = false;
-
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 600,
@@ -16,36 +14,28 @@ function createWindow() {
       contextIsolation: false
     }
   });
-
   mainWindow.loadFile('index.html');
-
   globalShortcut.register('CommandOrControl+Shift+X', () => {
     isClickThrough = !isClickThrough;
     mainWindow.setIgnoreMouseEvents(isClickThrough, { forward: true });
     mainWindow.webContents.send('toggle-mode', isClickThrough);
   });
-
-  // 新增：接收前端傳來的最小化與關閉指令
   ipcMain.on('window-minimize', () => {
     mainWindow.minimize();
   });
-
   ipcMain.on('window-close', () => {
     mainWindow.close();
   });
 }
-
 app.whenReady().then(() => {
   createWindow();
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
 });
-
 app.on('will-quit', () => {
   globalShortcut.unregisterAll();
 });
-
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
 });
